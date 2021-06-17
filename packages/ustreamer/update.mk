2390204c1f2f483b0e7eb4856360b998365f8e73
Devaev Maxim <mdevaev@gmail.com>
2021-06-17 19:51:25 +0300

    ustreamer == 4.4

--- packages/ustreamer/update.mk
+++ packages/ustreamer/update.mk
@@ -3,5 +3,5 @@ all:
 
 update:
 	curl -s https://raw.githubusercontent.com/pikvm/ustreamer/master/pkg/arch/PKGBUILD \
-			| sed -e "s/^depends=(/depends=(raspberrypi-firmware /g" \
+			| sed -e "s/^depends=(/depends=(raspberrypi-firmware janus-gateway-pikvm /g" \
 		> PKGBUILD
