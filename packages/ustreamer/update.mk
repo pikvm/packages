100664c4a3795aac72ed29114a7c20dc4e96686a
Maxim Devaev <mdevaev@gmail.com>
2023-10-22 09:37:47 +0300

    big os update

--- packages/ustreamer/update.mk
+++ packages/ustreamer/update.mk
@@ -3,6 +3,6 @@ all:
 
 update:
 	curl -s https://raw.githubusercontent.com/pikvm/ustreamer/master/pkg/arch/PKGBUILD \
-			| sed -e "s/^depends=(/depends=(raspberrypi-firmware janus-gateway-pikvm alsa-lib opus speexdsp /g" \
+			| sed -e "s/^depends=(/depends=(janus-gateway-pikvm alsa-lib opus speexdsp /g" \
 			| sed -e "s/(janus-gateway /(janus-gateway-pikvm /g" \
 		> PKGBUILD
