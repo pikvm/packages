2c1c3c9f57f14d23ad3a61192d268d29c999747a
Maxim Devaev <mdevaev@gmail.com>
2024-01-07 05:49:31 +0300

    libgpiod1

--- packages/ustreamer/update.mk
+++ packages/ustreamer/update.mk
@@ -5,4 +5,5 @@ update:
 	curl -s https://raw.githubusercontent.com/pikvm/ustreamer/master/pkg/arch/PKGBUILD \
 			| sed -e "s/^depends=(/depends=(janus-gateway-pikvm alsa-lib opus speexdsp /g" \
 			| sed -e "s/(janus-gateway /(janus-gateway-pikvm /g" \
+			| sed -e "s/\<libgpiod\>/libgpiod1/g" \
 		> PKGBUILD
