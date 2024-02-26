45792711a4e692c79db9a25ef28f37071b0d20e9
Maxim Devaev <mdevaev@gmail.com>
2024-02-26 23:48:08 +0300

    ustreamer == 5.53

--- packages/ustreamer/update.mk
+++ packages/ustreamer/update.mk
@@ -3,6 +3,7 @@ all:
 
 update:
 	curl -s https://raw.githubusercontent.com/pikvm/ustreamer/master/pkg/arch/PKGBUILD \
-			| sed -e "s/^depends=(/depends=(janus-gateway-pikvm alsa-lib opus speexdsp /g" \
+			| sed -e 's/^_options="/_options="WITH_V4P=1 /g' \
+			| sed -e "s/^depends=(/depends=(libdrm janus-gateway-pikvm alsa-lib opus speexdsp /g" \
 			| sed -e "s/(janus-gateway /(janus-gateway-pikvm /g" \
 		> PKGBUILD
