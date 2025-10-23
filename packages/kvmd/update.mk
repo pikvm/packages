4a3a140760d845f3164cac6c589d6207d176b180
Maxim Devaev <mdevaev@gmail.com>
2025-10-23 12:59:46 +0000

    kvmd == 4.106

--- packages/kvmd/update.mk
+++ packages/kvmd/update.mk
@@ -4,3 +4,4 @@ all:
 update:
 	curl -s https://raw.githubusercontent.com/pikvm/kvmd/master/PKGBUILD > PKGBUILD
 	curl -s https://raw.githubusercontent.com/pikvm/kvmd/master/kvmd.install > kvmd.install
+	curl -s https://raw.githubusercontent.com/pikvm/kvmd/master/platform.install > platform.install
